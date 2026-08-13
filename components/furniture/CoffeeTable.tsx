1b9232b84d16e98703df4d6d65abbe717f91bde1
hosoning <hosoning@gmail.com>
2026-08-13 10:22:07 +0800

    Replace coffee table with layered travertine design

--- components/furniture/CoffeeTable.tsx
+++ components/furniture/CoffeeTable.tsx
@@ -1,24 +1,14 @@
 'use client';
 
 export default function CoffeeTable({ position = [0, 0, 0], rotationY = 0 }: { position?: [number, number, number]; rotationY?: number }) {
-  const legOffsets: [number, number][] = [
-    [-0.42, -0.22],
-    [0.42, -0.22],
-    [-0.42, 0.22],
-    [0.42, 0.22],
-  ];
   return (
     <group position={position} rotation={[0, rotationY, 0]}>
-      <mesh position={[0, 0.36, 0]} castShadow receiveShadow>
-        <boxGeometry args={[0.95, 0.04, 0.55]} />
-        <meshStandardMaterial color="#c9c2b4" roughness={0.2} metalness={0.05} />
-      </mesh>
-      {legOffsets.map(([x, z]) => (
-        <mesh key={`${x}-${z}`} position={[x, 0.18, z]} castShadow>
-          <cylinderGeometry args={[0.02, 0.02, 0.36, 10]} />
-          <meshStandardMaterial color="#2b2320" metalness={0.6} roughness={0.4} />
-        </mesh>
-      ))}
+      <mesh position={[-0.22,0.33,0]} castShadow receiveShadow><cylinderGeometry args={[0.48,0.5,0.08,48]} /><meshStandardMaterial color="#d6c9b8" roughness={0.72} metalness={0.02} /></mesh>
+      <mesh position={[0.34,0.27,0.08]} castShadow receiveShadow><cylinderGeometry args={[0.34,0.36,0.07,48]} /><meshStandardMaterial color="#bfa98e" roughness={0.68} metalness={0.03} /></mesh>
+      <mesh position={[-0.22,0.16,0]} castShadow><cylinderGeometry args={[0.16,0.22,0.28,32]} /><meshStandardMaterial color="#6e5542" roughness={0.54} /></mesh>
+      <mesh position={[0.34,0.13,0.08]} castShadow><cylinderGeometry args={[0.12,0.17,0.23,28]} /><meshStandardMaterial color="#8b6d54" roughness={0.56} /></mesh>
+      <mesh position={[-0.06,0.39,-0.08]} rotation={[0.04,0.22,-0.04]} castShadow><boxGeometry args={[0.28,0.025,0.2]} /><meshStandardMaterial color="#6e4e38" roughness={0.8} /></mesh>
+      <mesh position={[0.12,0.4,0.02]}><cylinderGeometry args={[0.055,0.05,0.1,20]} /><meshPhysicalMaterial color="#d7d9d4" transmission={0.5} transparent opacity={0.75} roughness={0.18} /></mesh>
     </group>
   );
 }
