c6ebd5e20e045dc1a1d148101e85908bfe5c654a
hosoning <hosoning@gmail.com>
2026-08-13 14:03:17 +0800

    Add woven rug surface texture

--- components/furniture/Rug.tsx
+++ components/furniture/Rug.tsx
@@ -1,24 +1,37 @@
 'use client';
 
+import { useMemo } from 'react';
+import { createRugTexture } from '@/lib/textures';
+
 export default function Rug({
   position = [0, 0.005, 0] as [number, number, number],
   size = [2.6, 1.8] as [number, number],
-  color = '#8a3f3f',
+  color = '#b9aa98',
 }: {
   position?: [number, number, number];
   size?: [number, number];
   color?: string;
 }) {
+  const texture = useMemo(() => createRugTexture(256, color), [color]);
   return (
     <group position={position}>
       <mesh rotation={[-Math.PI / 2, 0, 0]} receiveShadow>
         <planeGeometry args={size} />
-        <meshStandardMaterial color={color} roughness={0.95} />
+        <meshStandardMaterial map={texture} roughness={1} />
       </mesh>
-      <mesh rotation={[-Math.PI / 2, 0, 0]} position={[0, 0.001, 0]}>
-        <planeGeometry args={[size[0] - 0.16, size[1] - 0.16]} />
-        <meshStandardMaterial color="#e8ddc8" roughness={0.95} />
+      <mesh rotation={[-Math.PI / 2, 0, 0]} position={[0, 0.002, 0]}>
+        <planeGeometry args={[size[0] - 0.18, size[1] - 0.18]} />
+        <meshStandardMaterial color="#d8ccbc" transparent opacity={0.12} roughness={1} />
       </mesh>
+      {Array.from({ length: 28 }).map((_, index) => {
+        const x = -size[0] / 2 + (index / 27) * size[0];
+        return (
+          <mesh key={index} position={[x, 0.003, size[1] / 2 + 0.025]} rotation={[-Math.PI / 2, 0, 0]}>
+            <planeGeometry args={[0.012, 0.07]} />
+            <meshStandardMaterial color="#b7a792" roughness={1} />
+          </mesh>
+        );
+      })}
     </group>
   );
 }
